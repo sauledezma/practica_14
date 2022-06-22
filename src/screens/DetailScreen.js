d578bb4f262278e888421675d335c2b268275cc1
sauledezma <s.ledezmaut2019@gmail.com>
2022-06-21 19:39:42 -0600

    primer comit

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,40 @@
+
+
+import React,{ useEffect, useState} from "react";
+import { Text,Button, SafeAreaView } from "react-native";
+export default function SettingsScreen({ route, navigation }) {  
+   
+    const { itemId } = route.params;  
+    const [post, setPost] = useState({});  
+
+   const getPost = async() => {
+
+    try{
+      const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;  
+      const response = await fetch(url);
+      const json = await response.json(); 
+      setPost(json);
+      console.log(url);
+    } catch (error) {
+      console.error(error);
+
+    }
+
+  }
+  useEffect(()=>{
+    getPost();
+  }, [itemId])
+
+  return (
+
+    <SafeAreaView>
+      <Text>Id: {JSON.stringify(itemId)}</Text>
+      <Text>Titulo: {post.title}</Text>
+      <Text>Body: {post.body}</Text>
+
+      <Button onPress={() =>{  
+        navigation.navigate('Home');
+      }} title="REGRESAR" />
+    </SafeAreaView>
+  );
+}
